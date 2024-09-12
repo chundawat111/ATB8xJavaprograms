5ffa3e63bb2299d32c7709d2356b698efdb90941
Admin <chundawat111@gmail.com>
2024-09-12 09:17:59 +0530

    Push

---
+++ src/Sept/ax09112024/Lab005.java
@@ -0,0 +1,15 @@
+package Sept.ax09112024;
+
+public class Lab005 {
+    public static void main(String[] Narendra) {
+// comments
+        // commets
+        /*comments
+        * fff
+        * fff
+        *        */
+        /** test
+         *
+         **/
+    }
+}
