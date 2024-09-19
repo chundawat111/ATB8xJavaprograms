ad29dee0220c604819c7749ba329ac8e02be726a
Admin <chundawat111@gmail.com>
2024-09-19 23:45:09 +0530

    Push

--- src/Sept/ax09192024/Lab007.java
+++ src/Sept/ax09192024/Lab007.java
@@ -3,7 +3,7 @@ package Sept.ax09192024;
 public class Lab007 {
     public static void main(String[] args) {
         short s = 10;
-        char c = 'A';
+        char c = 'A';//65
         int ss = s + c;
         system.out.println(ss);
     }
